9d3cdbd898e0c4520a47b13dcc10313451fde2d9
Nishan Bista <nishanbista525@gmail.com>
2024-04-08 22:31:14 +0545

    feat:copy password

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback, useRef, useEffect } from "react";
 
 export default function App() {
-
-  const [password,setPassword] = useState("")
+  const [password, setPassword] = useState("");
 
   const [copy, setCopy] = useState("Copy");
 
@@ -14,35 +13,31 @@ export default function App() {
 
   const [specialCharsAll, setSpecialCharsAll] = useState(false);
 
-  const generatePassword = useCallback(()=>{
-    let pass= ""
-    let str="abcdefghijklmnopqrstuvwxyz"
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyz";
 
-    if(capitalALl) str+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if(numbersAll) str+="0123456789"
-    if(specialCharsAll) str+="!@#$%^&*_-<>?/"
+    if (capitalALl) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numbersAll) str += "0123456789";
+    if (specialCharsAll) str += "!@#$%^&*_-<>?/";
 
-   
-    for(let i = 1;i<=length;i++ ){
-      let char = Math.floor(Math.random() * str.length + 1)
-      pass+=str.charAt(char)
-
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
 
-    setPassword(pass)
-    
-  },[capitalALl,length,numbersAll,specialCharsAll])
+    setPassword(pass);
+  }, [capitalALl, length, numbersAll, specialCharsAll]);
 
+  const handleCopy = () => {
+    window.navigator.clipboard.writeText(password);
+    setCopy("Copied");
+  };
 
-
-  useEffect(()=>{
-    setCopy("Copy")
-    generatePassword()
-    
-
-
-
-  },[capitalALl,length,numbersAll,specialCharsAll])
+  useEffect(() => {
+    setCopy("Copy");
+    generatePassword();
+  }, [capitalALl, length, numbersAll, specialCharsAll]);
 
   return (
     <>
@@ -64,9 +59,7 @@ export default function App() {
               className="border-r-[2px] border-t-[2px] border-b-[2px] border-five text-three px-[15px] rounded-r-lg
               hover:bg-slate-500 active:bg-slate-100 active:text-black
               "
-              onClick={() => {
-                setCopy("Copied");
-              }}
+              onClick={handleCopy}
             >
               {copy}
             </button>
